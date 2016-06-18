948e3e51eafae109f08716661677c7328ea4e56a
Vive PC <hanafusa@lel.hiroshima-u.ac.jp>
2016-06-19 01:26:22 +0900

    右手コントローラのトリガーボタンを押せばレーザー（ポインター）が出る

---
+++ Assets/Main/Scripts/VRController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class VRController: MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.enabled = false;
+    }
+
+    void Update()
+    {
+        SteamVR_TrackedObject trackedObject = GetComponent<SteamVR_TrackedObject>();
+        var device = SteamVR_Controller.Input((int)trackedObject.index);
+  
+        if (device.GetPress(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("トリガーを深く引いている");
+            lineRenderer.enabled = true;
+        }
+        else
+        {
+           lineRenderer.enabled = false;
+        }
+ 
+    }
+}
